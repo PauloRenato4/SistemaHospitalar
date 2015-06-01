a980b9be96833fc1ce7e6e722107b33bd7ecccc8
jdiegop <diego_turvo@hotmail.com>
2015-06-01 13:27:55 -0300

    receita

---
+++ js/receita.js
@@ -0,0 +1,6 @@
+ function dateNow(span) {
+        
+    now new Date
+document.write ("Data: "  + now.getDate() + "/" + now.getMonth() + "/" + now.getFullYear() + "Horas " + now.getHours() + "Minutos"+ now.getMinutes() + now.getSeconds())
+    
+}
