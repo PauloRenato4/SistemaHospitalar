f10bd679e3460a47437dc3c43c1df6d5e9be5e9d
jdiegop <diego_turvo@hotmail.com>
2015-06-10 15:48:05 -0300

    formulario de Receituario e exame corrigido

---
+++ js/date.js
@@ -0,0 +1,10 @@
+ function dateNow() {
+        
+  var now = new Date();
+document.write ("Data:"  + now.getDate() + " / " + now.getMonth() + " / " + now.getFullYear()+ "   " + now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds());
+     
+     
+     
+    
+   
+}
--- js/receita.js
+++
@@ -1,6 +0,0 @@
- function dateNow(span) {
-        
-    now new Date
-document.write ("Data: "  + now.getDate() + "/" + now.getMonth() + "/" + now.getFullYear() + "Horas " + now.getHours() + "Minutos"+ now.getMinutes() + now.getSeconds())
-    
-}
