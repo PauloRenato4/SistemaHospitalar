21a6fe80615d9e29132c35bfd23797076e6a6343
Paulo Renato Moreira Paschoal <paulo.dimaria34@hotmail.com>
2015-06-16 16:36:31 -0300

    grafico

---
+++ php/grafico.php
@@ -0,0 +1,56 @@
+<?php
+// Consultar dados
+$dados = array();
+
+$pdo = new PDO('mysql:host=localhost;dbname=sistemashospitalar', 'root', 'root');
+$sql = 'SELECT sexo, COUNT(*) as quantidade FROM paciente GROUP BY sexo';
+$stmt = $pdo->query($sql);
+
+while ($obj = $stmt->fetchObject()) {
+    switch ($obj->sexo) {
+    case 'M':
+        $dados['Homens'] = $obj->quantidade;
+        break;
+    case 'F':
+        $dados['Mulheres'] = $obj->quantidade;
+        break;
+    }
+}
+
+echo gerar_grafico_torta($dados, 'Quantidade de Homens/Mulheres');
+exit(0);
+
+/**
+ * Imprime um grafico de torta
+ * @param array[string => int] $dados: Dados do Grafico
+ * @param string $titulo: Titulo do grafico
+ * @param int $largura: Largura do grafico
+ * @param int $altura: Altura do grafico
+ * @return string Tag IMG com o caminho para o grafico
+ */
+function gerar_grafico_torta($dados, $titulo, $largura = 250, $altura = 100) {
+
+    // Gerando a URL dinamicamente
+    $labels = array_keys($dados);
+    $valores = array_values($dados);
+
+    // Converter valores para porcentagens
+    $soma = array_sum($valores);
+    $percentual = array();
+    foreach ($valores as $valor) {
+        $percentual[] = round($valor * 100 / $soma);
+    }
+
+    $grafico = array(
+        'cht'  => 'p3',
+        'chs'  => $largura.'x'.$altura,
+        'chd'  => 't:'.implode(',', $percentual),
+        'chl'  => implode('|', $labels)
+    );
+    $url = 'https://chart.googleapis.com/chart?'.http_build_query($grafico, '', '&');
+
+    // Imprimindo o gráfico
+    return sprintf('<img src="%s" width="%d" height="%d" alt="%s" />',
+        $url, $largura, $altura, htmlentities($titulo, ENT_COMPAT, 'UTF-8')
+    );
+}
