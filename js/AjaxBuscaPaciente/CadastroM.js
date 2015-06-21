7afe2738596d9ec3d5000e342c474cdcbef05120
Paulo Renato Moreira Paschoal <paulo.dimaria34@hotmail.com>
2015-06-20 23:14:59 -0300

    cadastrar Atedente e Medico js e php

---
+++ js/AjaxBuscaPaciente/CadastroM.js
@@ -0,0 +1,64 @@
+var cadPerson = {
+    
+    init: function () {
+        cadPerson.readPage();
+    },
+    
+    readPage: function () {
+        
+        var form = document.getElementById('cadastrar');
+        
+        form.addEventListener('submit', function (event) {
+                                            var data = cadPerson.sendValues();
+                                            event.preventDefault();
+                                            
+                                        }
+                             )
+    },
+    
+    sendValues: function () {
+
+var cpf = $("#cpf").val();
+        var nome = $("#nome").val();
+        var sobrenome = $("#sobrenome").val();
+        var rg = $("#rg").val();
+        var nacionalidade = $("#nacionalidade").val();
+        var email = $("#email").val();
+        var nascimento = $("#nascimento").val();
+        var idade = $("#idade").val();
+        var ddd = $("#ddd").val();
+        var celular = $("#celular").val();
+        var sexo = $("#sexo").val();
+        var tipo = $("#tipo").val();
+        var logradouro = $("#logradouro").val();
+        var numero = $("#numero").val();
+        var bairro = $("#bairro").val();
+        var estado = $("#estado").val();
+        var cidade = $("#cidade").val();
+        var cep = $("#cep").val();
+        var crm = $("#crm").val();
+        var checke = $("#checke").val();
+        var mensagem = $("#mensagem").val();
+        var expedicao = $("#expedicao").val();
+        var turno = $("#turno").val();
+        
+        $.post('php/CadastroMedico.php',
+               {cpf: cpf, nome: nome, sobrenome: sobrenome, rg: rg, nacionalidade: nacionalidade, email: email,
+                nascimento: nascimento, idade: idade, ddd: ddd, celular: celular, sexo: sexo, tipo: tipo, logradouro: logradouro, numero: numero,
+                bairro: bairro, estado: estado, cidade: cidade, cep: cep, crm: crm, checke: checke, mensagem: mensagem, expedicao: expedicao, turno: turno},
+               function(data){
+                    cadPerson.report(data);
+                });
+    },
+    
+    report: function(data) {
+        
+        if(data=='success')
+            alert('Cadastro Realizado com sucesso!');
+        
+        else
+            alert(data);
+    }
+}
+
+cadPerson.init();
