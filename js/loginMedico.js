e11522be9db111999c05d265454354f7963cec9f
Paulo Renato Moreira Paschoal <paulo.dimaria34@hotmail.com>
2015-06-22 23:35:27 -0300

    login medico

--- js/loginMedico.js
+++ js/loginMedico.js
@@ -1,11 +1,12 @@
 
+
 function pasuser(form) {
 if (form.id.value=="medico") { 
 if (form.pass.value=="2015") {              
 location="medicohospital.html" 
 } else {
-alert("Nome ou Senha Invalida!")
+alert("Senha Invalida!")
 }
-} else {  alert("Nome ou Senha Invalido!")
+} else {  alert("Nome Invalido!")
 }
 }
