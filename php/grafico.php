17b6496043bcb017546bb42ad39678f6d2d4f8b9
Paulo Renato Moreira Paschoal <paulo.dimaria34@hotmail.com>
2015-11-21 20:40:11 -0200

    teste

--- php/grafico.php
+++ php/grafico.php
@@ -2,18 +2,18 @@
 
 $pdo = new PDO('mysql:host=localhost;dbname=u350455626_siste', 'u350455626_siste', 'univas35');
 
-$sql = 'SELECT sexo as genero, COUNT(M) as quantidade FROM medico where sexo = M GROUP BY sexo';
+$sql = 'SELECT sexo , COUNT(*) as quantidade FROM medico where  GROUP BY sexo';
  $table = array();
  $table['cols'] = array(
     
-    array('label' => 'genero', 'type' => 'string'),
+    array('label' => 'sexo', 'type' => 'string'),
     array('label' => 'quantidade', 'type' => 'number')
 );
 $rows = array();
 while($r = mysql_fetch_assoc($sql)) {
     $temp = array();
     // each column needs to have data inserted via the $temp array
-    $temp[] = array('v' => $r['genero']);
+    $temp[] = array('v' => $r['sexo']);
     $temp[] = array('v' => (int) $r['quantidade']); // typecast all numbers to the appropriate type (int or float) as needed - otherwise they are input as strings
     
     // insert the temp array into $rows
